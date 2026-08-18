211ee895b4273c63b2df934d1b2e83f37ca138dd
Chika Ugorji <chikaugorji@outlook.com>
2026-08-18 00:15:49 -0700

    added more routes

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ const app = new Hono()
 
 const text_strings = ['Heyyy bihh why you using my api? huh...', 'oouu shii secret route oml']
 
+const tasks = ['do laundry']
 app.get('/', (c) => {
   return c.text(text_strings[0])
 })
@@ -23,5 +24,28 @@ app.get('/hello/:name', (c) => {
   const name = c.req.param('name')
   return c.json(`hello ${name} u look kawaii today`)
 })
+
+app.get('/tasks', (c) => {
+  return c.json(tasks)
+})
+
+app.get('/task/:id', (c) => {
+  const id = c.req.param('id')
+  const task = tasks[Number(id)]
+  return c.json(task)
+})
+
+app.post('/tasks', async (c) => {
+  const body = await c.req.json()
+
+  tasks.push(body.title)
+  return c.json({message: 'task created sucessfully', tasks}, 201)
+})
+
+app.delete('/tasks/:id', (c) => 
+  c.text(`${c.req.param('id')} is deleted`)
+)
+
+
 
 export default app
